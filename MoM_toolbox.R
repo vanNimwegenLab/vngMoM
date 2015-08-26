599d58c6a2688e1891a1653d33834d3e50f8f211
Thomas Julou <thomas.julou@gmail.com>
2015-08-26 13:34:32 +0200

    MoM refactoring to match Erik's new syntax

--- MoM_toolbox.R
+++ MoM_toolbox.R
@@ -22,20 +22,22 @@ scale_shape_periodic <-
 
 
 load_timm_data <- function(.path, .perl_cmd='perl',
-                           .frames_script_path="/import/bc2/home/nimwegen/GROUP/MM_Analysis/scripts/get_height_size_intens.pl", 
-                           .cells_script_path="/import/bc2/home/nimwegen/GROUP/MM_Analysis/scripts/estimate_division_growth2.pl", 
+                           .frames_script_path="/import/bc2/home/nimwegen/GROUP/MM_Analysis/scripts/get_size_and_fluo.pl", 
+                           .cells_script_path="/import/bc2/home/nimwegen/GROUP/MM_Analysis/scripts/estimate_division_growth.pl", 
                            .force=FALSE, .verbose=FALSE,
                            .data2preproc=identity) {
   if (!file.exists(.path)) stop("input path is not valid.")
-  .out_frames_path <- .path %>% .data2preproc %>% file_path_sans_ext %>% paste0("_frames.txt")
-  .out_cells_path <- .path %>% .data2preproc %>% file_path_sans_ext %>% paste0("_cells.txt")
-  dir.create(dirname(.out_frames_path), recursive=TRUE, showWarnings=FALSE)
+  .dir <- dirname(.path)
+  .outdir <- .dir %>% .data2preproc
+  dir.create(.outdir, recursive=TRUE, showWarnings=FALSE)
+  .outbase <- basename(.path) %>% sub("ExportedCellStats_", "", .) %>% file_path_sans_ext
+  .out_frames_path <- paste0(.outbase, "_frames.txt") %>% file.path(.outdir, .)
+  .out_cells_path <- paste0(.outbase, "_cells.txt") %>% file.path(.outdir, .)
   
   # run perl scripts if output doesn't exist
   if (!file.exists(.out_frames_path) || .force==TRUE) {
     if (.verbose) print(paste("Converting", .path, "to", .out_frames_path))
-#     browser()
-    system2(.perl_cmd, args=c(.frames_script_path, .path), stdout=.out_frames_path)
+    system2(.perl_cmd, args=c(.frames_script_path, .path, "0"), stdout=.out_frames_path)
   }
   if (!file.exists(.out_frames_path)) stop("Frame file cannot be found.")
 
@@ -44,11 +46,17 @@ load_timm_data <- function(.path, .perl_cmd='perl',
     system2(.perl_cmd, args=c(.cells_script_path, .out_frames_path), stdout=.out_cells_path)
   }
   
-  .cells_out <- read.table(.out_cells_path, sep=" ", comment.char="", header=TRUE)
-  names(.cells_out) <- gsub("X\\.", "", names(.cells_out))
-  .cells_out <- mutate(.cells_out, cid = compute_genealogy(id, parent_id, daughter_type))
+  .cells_out <- read.table(.out_cells_path, comment.char="", header=TRUE)
+  names(.cells_out) <- names(.cells_out) %>% 
+    gsub("X\\.", "", .) %>%
+    gsub("\\.", "_", .)
+  .cells_out <- .cells_out %>%
+    rename(id=cellID, parent_id=parID) %>%
+    mutate(cid = compute_genealogy(id, parent_id, daughter_type))
   .frames_out <- parse_frames_stats(.out_frames_path) %>%
-    inner_join(.cells_out, ., by="id")
+    select(-daughter_type, -type_of_end) %>%
+    rename(id=cell_ID, parent_id=parent_ID) %>%
+    left_join(.cells_out)
   if (dim(.cells_out)[1] > 0) {
     .m <- str_match(basename(.path), ".*(\\d{8})_.*pos(\\d+).*_GL(\\d+).*")
     return(list(cells=data.frame(date=as.numeric(.m[1, 2]), pos=as.numeric(.m[1, 3]), gl=as.numeric(.m[1, 4]), .cells_out),
@@ -61,23 +69,25 @@ as_numeric_df <- function(.d) modifyList(.d, lapply(.d, as_numeric))
 
 parse_frames_stats <- function(.path) {
   flines <- readLines(.path)
-  # browser()
 
   # find column names for stats
-  .m <- str_match(flines[1], ".* STATS: (.*)$")
-  .colnames <- str_split(.m[1,2], '\t')[[1]]
+  .fieldnames <- str_split(flines[1], '\t')[[1]] %>% sub("#", "", .) %>%
+    gsub("-", "_", .)
+  .colnames <- str_split(flines[2], '\t')[[1]] %>% sub("#", "", .) %>%
+    gsub("-", "_", .)
   
   # parse all cells
-  id_lines <- grep("^>cell_", flines)
+  id_lines <- grep("^>CELL", flines)
   .out <- do.call(rbind, 
                   lapply(seq_along(id_lines)[-1], function(.i) {
                     .df <- do.call(rbind, 
                                    strsplit(flines[(id_lines[.i-1]+1):(id_lines[.i]-1)], '\t'))
                     colnames(.df) <- .colnames
                     .info_str <- flines[id_lines[.i-1]]
-                    .info_spl <- strsplit(.info_str, '_')[[1]]
-                    # data.frame(id=.info_spl[2], parent_id=.info_spl[4], birthframe=.info_spl[6], endframe=.info_spl[8], type_of_end=.info_spl[10], .df)
-                    data.frame(id=.info_spl[2], .df)
+                    .info_spl <- strsplit(.info_str, '\t')[[1]]
+                    .info <- data.frame(t(.info_spl[-1])) %>%
+                      setNames(.fieldnames[-1])
+                    cbind(.info, .df)
                   }) )
   return(as_numeric_df(.out))
 }
