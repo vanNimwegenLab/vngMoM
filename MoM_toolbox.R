2c3c1eefff59363c9e9071de0bcf554e69d52668
Thomas Julou <thomas.julou@gmail.com>
2016-04-12 17:45:25 +0200

    - improved mother_machine toolbox - added an example for mother_machine - upgraded geom_stephist (renamed geom_step_hist) to work with ggplot2 2.0

--- MoM_toolbox.R
+++ MoM_toolbox.R
@@ -1,32 +1,79 @@
-suppressPackageStartupMessages( {
-  library(Hmisc)
-  library(tools)
-  library(stringr)
-  library(tidyr)
-  library(dplyr)
-  
-  library(ggplot2)
-  library(scales)
-  library(RColorBrewer)
-} )
+mylibs <- c("stats", # required to prevent hiding dplyr::filter later on
+            "tools", "stringi", "stringr", "tidyr", "dplyr",
+            "ggplot2", "scales", "RColorBrewer", "gridExtra",
+            "RcppArmadillo", "ccaPP")
+invisible( suppressPackageStartupMessages( # don't use %>% before loading dplyr
+  lapply(mylibs, library, character.only=TRUE) ))
 
+# ggplot2 housekeeping ####
 theme_set(theme_bw())
 scale_colour_discrete <- function(...) scale_colour_brewer(..., palette="Set1", na.value='gray50')
 scale_fill_discrete <- function(...) scale_fill_brewer(..., palette="Set1", na.value='gray50')
 # to revert to the default ggplot2 discrete colour scale, use: + ggplot2::scale_colour_discrete()
+brewer_cols <- c(brewer.pal(4, 'Set1'), 'gray42')
 scale_colour_periodic_brewer <-
-  function(...) scale_colour_manual(..., values = rep(c(brewer.pal(4, 'Set1'), 'gray42'), 1000), na.value='gray25')
+  function(...) scale_colour_manual(..., values = rep(brewer_cols, 1000), na.value='gray25')
 scale_fill_periodic_brewer <- function(...) 
-  scale_fill_manual(..., values = rep(c(brewer.pal(4, 'Set1'), 'gray42'), 1000), na.value='gray25')
+  scale_fill_manual(..., values = rep(brewer_cols, 1000), na.value='gray25')
 scale_shape_periodic <- function(...) 
   scale_shape_manual(..., values = rep(15:18, 5))
 
 formatter_sec_to_h <- function(.x) .x/3600 #%>% format(digits=2)
-scale_x_hours <- function(.dh=6, ...) scale_x_continuous(..., name='time (h)', labels=formatter_sec_to_h,
-                                                  breaks=function(.lims) seq(.lims[1] %/% (.dh*3600) *.dh*3600, (.lims[2] %/% (.dh*3600) + 1) *.dh*3600, .dh*3600))
+scale_x_hours <- function(.dh=6, ...) {
+  .f_breaks <- function(.lims) 
+    seq(.lims[1] %/% (.dh*3600) *.dh*3600, (.lims[2] %/% (.dh*3600) + 1) *.dh*3600, .dh*3600)
+  .call <- as.list(match.call()) # avoid pipe
+  if("name" %in% names(.call)) {
+    scale_x_continuous(..., labels=formatter_sec_to_h, breaks=.f_breaks)
+  } else {
+    scale_x_continuous(..., name="time (h)", labels=formatter_sec_to_h, breaks=.f_breaks)
+  }
+}
+scale_y_hours <- function(.dh=6, ...) {
+  .f_breaks <- function(.lims) 
+    seq(.lims[1] %/% (.dh*3600) *.dh*3600, (.lims[2] %/% (.dh*3600) + 1) *.dh*3600, .dh*3600)
+  .call <- as.list(match.call()) # avoid pipe
+  if("name" %in% names(.call)) {
+    scale_y_continuous(..., labels=formatter_sec_to_h, breaks=.f_breaks)
+  } else {
+    scale_y_continuous(..., name="time (h)", labels=formatter_sec_to_h, breaks=.f_breaks)
+  }
+}
 sec_to_h_trans <- function() trans_new("sec_to_h", function(.x) .x/3600, function(.x) .x*3600)
 
+# multidplyr utility ####
+# cluster_assign_obj <- function(cl, x) {
+#   name <- as.character(substitute(x))
+#   stopifnot(exists(name))
+#   cluster_assign_value(cl, name, x)
+# }
+cluster_assign_obj <- function(.cl, ...) {
+  .xs <- list(...)
+  .ns <- as.character(eval(substitute(alist(...))))
+  for (.i in 1:length(.xs)) {
+    stopifnot(exists(.ns[[.i]]))
+    .cl <- cluster_assign_value(.cl, .ns[[.i]], .xs[[.i]])
+  }
+  return(.cl)
+}
 
+# cluster_assign_func <- function(cl, f) {
+#   fname <- as.character(substitute(f))
+#   stopifnot(is.function(f))
+#   cluster_assign_value(cl, fname, f)
+# }
+cluster_assign_func <- function(.cl, ...) {
+  .fs <- list(...)
+  .ns <- as.character(eval(substitute(alist(...))))
+  for (.i in 1:length(.fs)) {
+    stopifnot(is.function(.fs[[.i]]))
+    .cl <- cluster_assign_value(.cl, .ns[[.i]], .fs[[.i]])
+  }
+  return(.cl)
+}
+
+
+# general convenience functions ####
 between_or <- function(.x, .a, .b) {
   if (length(.a) != length(.b)) error('.a and .b must have the same length.')
   lapply(seq(length(.a)), function(.i) between(.x, .a[.i], .b[.i])) %>%
@@ -37,6 +84,16 @@ between_or <- function(.x, .a, .b) {
 as_numeric <- function(.x) as.numeric(as.character(.x))
 as_numeric_df <- function(.d) modifyList(.d, lapply(.d, as_numeric))
 
+mycut <- function(.x, ...) {
+  .k <- Hmisc::cut2(.x, ...)
+  .mids <- sapply(str_match_all(as.character(levels(.k)), "[\\[\\(](.*),(.*)[\\]\\)]"), 
+                 function(.m) {
+                   mean(c(as.numeric(.m[2]), as.numeric(.m[3])))
+                 })
+  .mids[.k]
+}
+
+# MoMA loading ####
 load_timm_data <- function(.path, .scripts_path, 
                            .perl_cmd='perl',
                            .frames_script="get_size_and_fluo_basic.pl", 
@@ -53,7 +110,6 @@ load_timm_data <- function(.path, .scripts_path,
   .outbase <- basename(.path) %>% sub("ExportedCellStats_", "", .) %>% file_path_sans_ext
   .frames_path <- paste0(.outbase, "_frames.txt") %>% file.path(.outdir, .)
   # .cells_path <- paste0(.outbase, "_cells.txt") %>% file.path(.outdir, .)
-  # browser()
 
   # run perl scripts if output doesn't exist
   if (!file.exists(.frames_path) || .force==TRUE) { # || !file.exists(.cells_path) 
@@ -117,10 +173,16 @@ parse_frames_stats <- function(.path) {
   id_lines <- grep("^>CELL", flines)
   .out <- do.call(rbind, 
                   lapply(seq_along(id_lines)[-1], function(.i) {
-                    .df <- (id_lines[.i-1] + 1):(id_lines[.i] - 1) %>% # retrieve lines indices
-                      flines[.] %>% paste(collapse='\n') %>%           # concat lines
-                      textConnection %>% read.table(comment.char="", header=FALSE) %>% # read table
-                      setNames(.colnames)
+                    # browser()
+#                     .df <- (id_lines[.i-1] + 1):(id_lines[.i] - 1) %>% # retrieve lines indices
+#                       flines[.] %>% paste(collapse='\n') %>%           # concat lines
+#                       textConnection %>% read.table(comment.char="", header=FALSE) %>% # read table
+#                       setNames(.colnames)
+                    .str <- (id_lines[.i-1] + 1):(id_lines[.i] - 1) %>% # retrieve lines indices
+                      flines[.] %>% paste(collapse='\n') # concat lines
+                    .con <- textConnection(.str)
+                    .df <- read.table(.con, comment.char="", header=FALSE) %>% setNames(.colnames)
+                    close(.con) # close text connection
                     .info <- flines[id_lines[.i-1]] %>% # extract line
                       strsplit('[ \t]+') %>% .[[1]] %>%     # split to vector
                       .[-1] %>% t %>% data.frame(stringsAsFactors=FALSE) %>%    # convert to df
@@ -132,6 +194,60 @@ parse_frames_stats <- function(.path) {
   return(.out)
 }
 
+
+which_touch_exit <- function(.h, .hmin_cutoff) {
+  # browser()
+  .t <- which(.h<.hmin_cutoff)
+  if (length(.t) == 0) {
+    return(rep(FALSE, length(.h)))
+  } else {
+    return(c(rep(FALSE, .t[1]-1),
+             rep(TRUE, length(.h)-.t[1]+1)))
+  }
+}
+
+which_to_progeny <- function(.x, .cid) {
+  #   browser()
+  .df <- data.frame(x=.x, cid=.cid)
+  .out <- .x
+  .cs <- unique(.cid[which(.x)])
+  for (.c in .cs) {
+    .is_daughter <- str_detect(.cid, paste0(.c, ".+"))
+    .out[.is_daughter] <- TRUE
+  }
+  return(.out)  
+}
+
+
+parse_timm_curation <- function(.path) {
+  .flines <- readLines(.path)
+  .flines <- .flines[6:length(.flines)] %>%
+    gsub('\t', '', .) # remove tabs
+  # read one table for each type and store all tables in a list
+  .id_lines <- grep("^#", .flines)
+  .ldata <- lapply(seq(length(.id_lines)), function(.i) {
+    .start <- .id_lines[.i] + 1
+    .stop <- ifelse(.i==length(.id_lines), length(.flines), .id_lines[.i+1] - 1)
+    if (.start <= .stop)
+      return ( .flines[.start:.stop] %>% paste(collapse='\n') %>%                             # concat lines
+                 textConnection %>% read.table(comment.char="", sep=",", header=FALSE, stringsAsFactors=FALSE) )
+  })
+  if (all(sapply(.ldata, is.null))) return(data.frame(type='none', frame=-1, action=NA))
+  # find the width of the largest table
+  .max_cols <- lapply(.ldata, function(.d) dim(.d)[2]) %>% unlist %>% max
+  # increase the width of narrower dfs (in order to be able to stack them)
+  lapply(.ldata, function(.d) {
+    if (is.null(.d) || dim(.d)[2] == .max_cols) return(.d)
+    .d[1, .max_cols] <- NA # add NAs columns if narrower dataframe
+    .d
+  }) %>% 
+    # stack all tables
+    do.call(rbind, .) %>%
+    setNames(c('type', 'frame', 'xx', 'action'))
+}
+
+
+# genealogy utilities ####
 compute_genealogy <- function(.id, .pid, .dgtype) {
 # browser()
   # keep only one row per cell
@@ -170,55 +286,59 @@ get_daughters_cid <- function(.cid) {
   paste0(.cid, c('B', 'T'))
 }
 
+genealogy_ontology <- function(.div_min, .div_max) {
+  .rel <- NA
+  if (.div_min == 0 && .div_max == 1) {
+    .rel <- 'daughter'
+  } else if (.div_min == 0 && .div_max == 2) {
+    .rel <- 'gd-daughter'
+  } else if (.div_min == 0 && .div_max == 3) {
+    .rel <- 'gd-gd-daughter'
+  } else if (.div_min == 1 && .div_max == 1) {
+    .rel <- 'sisters'
+  } else if (.div_min == 2 && .div_max == 2) {
+    .rel <- 'cousins1'
+  } else if (.div_min == 3 && .div_max == 3) {
+    .rel <- 'cousins2'
+  } else if (.div_min == 1 && .div_max == 2) {
+    .rel <- 'niece'
+  }
+  return(.rel)
+}
 
-which_touch_exit <- function(.h, .hmin_cutoff) {
+genealogy_relationship <- function(.s, .dist_max=3) {
+  # sisters: div_min == div_max == 1
+  # 1st cousins: div_min == div_max == 2
+  # niece: div_min==1 && div_max==2
+
   # browser()
-  .t <- which(.h<.hmin_cutoff)
-  if (length(.t) == 0) {
-    return(rep(FALSE, length(.h)))
+  if (length(.s)!=2) stop("the argument must be a vector of 2 strings.")
+  .l1 <- str_length(.s[1])
+  .l2 <- str_length(.s[2])
+  if (abs(.l2 - .l1) > .dist_max) 
+    return(NULL)
+  
+  .min_l <- min(.l1, .l2)
+  .ss1 <- str_sub(.s[1], 1, .min_l)
+  .ss2 <- str_sub(.s[2], 1, .min_l)
+  if (.ss1 == .ss2) {
+    .anc_length <- .min_l
   } else {
-    return(c(rep(FALSE, .t[1]-1),
-             rep(TRUE, length(.h)-.t[1]+1)))
+  .x1 <- .ss1 %>% str_split("") %>% unlist
+  .x2 <- .ss2 %>% str_split("") %>% unlist
+  .anc_length <- min(which(.x1 != .x2)) - 1 # 0 if no common ancestor
   }
+  if (.min_l - .anc_length > .dist_max)
+    return(NULL)
+  .div_1 <- str_sub(.s[1], .anc_length+1, -1) %>% str_length # -1 to select the last char
+  .div_2 <- str_sub(.s[2], .anc_length+1, -1) %>% str_length # -1 to select the last char
+  .div_min <- min(.div_1, .div_2)
+  .div_max <- max(.div_1, .div_2)
+  if (.div_max > .dist_max) 
+    return(NULL)
+  return(list(rel=genealogy_ontology(.div_min, .div_max), ancestor=str_sub(.s[1], 1, .anc_length), 
+              genealogy_1=.s[1], div_1=.div_1, genealogy_2=.s[2], div_2=.div_2, 
+              div_min=.div_min, div_max=.div_max, less_div=which.min(c(.div_1, .div_2)) ))
 }
 
-which_to_progeny <- function(.x, .cid) {
-#   browser()
-  .df <- data.frame(x=.x, cid=.cid)
-  .out <- .x
-  .cs <- unique(.cid[which(.x)])
-  for (.c in .cs) {
-    .is_daughter <- str_detect(.cid, paste0(.c, ".+"))
-    .out[.is_daughter] <- TRUE
-  }
-  return(.out)  
-}
-
-
-parse_timm_curation <- function(.path) {
-  .flines <- readLines(.path)
-  .flines <- .flines[6:length(.flines)] %>%
-    gsub('\t', '', .) # remove tabs
-  # read one table for each type and store all tables in a list
-  .id_lines <- grep("^#", .flines)
-  .ldata <- lapply(seq(length(.id_lines)), function(.i) {
-    .start <- .id_lines[.i] + 1
-    .stop <- ifelse(.i==length(.id_lines), length(.flines), .id_lines[.i+1] - 1)
-    if (.start <= .stop)
-      return ( .flines[.start:.stop] %>% paste(collapse='\n') %>%                             # concat lines
-                 textConnection %>% read.table(comment.char="", sep=",", header=FALSE, stringsAsFactors=FALSE) )
-  })
-  if (all(sapply(.ldata, is.null))) return(data.frame(type='none', frame=-1, action=NA))
-  # find the width of the largest table
-  .max_cols <- lapply(.ldata, function(.d) dim(.d)[2]) %>% unlist %>% max
-  # increase the width of narrower dfs (in order to be able to stack them)
-  lapply(.ldata, function(.d) {
-    if (is.null(.d) || dim(.d)[2] == .max_cols) return(.d)
-    .d[1, .max_cols] <- NA # add NAs columns if narrower dataframe
-    .d
-  }) %>% 
-    # stack all tables
-    do.call(rbind, .) %>%
-    setNames(c('type', 'frame', 'xx', 'action'))
-}
 
