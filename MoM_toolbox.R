caa564333b84fee6e7065c02b85a822b2c0ad7a8
Thomas Julou <thomas.julou@gmail.com>
2016-10-05 18:10:04 +0200

    MoM: improved lag inference (under work)

--- MoM_toolbox.R
+++ MoM_toolbox.R
@@ -65,9 +65,11 @@ cluster_assign_func <- function(.cl, ...) {
 }
 
 # general convenience functions ####
-find.files <- function(.path, .name="", .pattern="", .mindepth=NULL, .maxdepth=NULL) {
+find.files <- function(.path, .name="", .pattern="", .mindepth=NULL, .maxdepth=NULL, .follow_symlinks=FALSE) {
 # find.files is a minimalist wrapper around bash's `find` to be used as a faster alternative to list.files
+# NB: setting .follow_symlinks=TRUE can hugely slow down the function
   .fargs <- .path
+  if (.follow_symlinks) .fargs <- c("-L", .fargs)
   if (.name!="") .fargs <- c(.fargs, paste("-name", .name))
   if (.pattern!="") .fargs <- c(.fargs, "-regextype posix-basic", paste("-regex", .pattern))
   if (!is.null(.mindepth)) .fargs <- c(.fargs, paste("-mindepth", .mindepth))
@@ -514,7 +516,6 @@ plot_faceted_var_tracks <- function(.df, .var_col='gfp_nb', .time_col='time_sec'
 
 # lag inference (from Athos) ####
 compute_lag <- function(.xs, .skip_start=0, .skip_end=1, .scaling_factor=1e-2, .plot=FALSE) {
-  # browser()
   if (length(.xs) < 3+.skip_start+.skip_end ) return(list())
   if (.skip_end<1) .skip_end <- 1
   
@@ -531,34 +532,37 @@ compute_lag <- function(.xs, .skip_start=0, .skip_end=1, .scaling_factor=1e-2, .
   .xsc <- .xs # scaled variable
   .T <- length(.xs);
   .taus <- (1+.skip_start):(.T-.skip_end) # remove very unlikely taus (for speedup)
-  # .maxLikLag <- NA
-  # while(any(is.na(.maxLikLag))) {
-    .xsc <- .xsc * .scaling_factor
-    .zs <- .xs-mean(.xsc) # compute z-transformed var
-    .DELTA_vecs <- lapply(.taus, function(.tau) 
-      ifelse((1:.T)<.tau, -(.T-.tau)*(1+.T-.tau)/(2*.T), (1:.T) -.tau-(.T-.tau)*(1+.T-.tau)/(2*.T)) )
-    .maxLikLag <- sapply(seq_along(.taus), function(.i) UnifLagInt(.T, .taus[.i], .zs, .DELTA_vecs[[.i]]))
-  # }
+  .xsc <- .xsc * .scaling_factor
+  .zs <- .xsc-mean(.xsc) # compute z-transformed var
+  .DELTA_vecs <- lapply(.taus, function(.tau) 
+    ifelse((1:.T)<.tau, -(.T-.tau)*(1+.T-.tau)/(2*.T), (1:.T) -.tau-(.T-.tau)*(1+.T-.tau)/(2*.T)) )
+  .maxLikLag <- sapply(seq_along(.taus), function(.i) UnifLagInt(.T, .taus[.i], .zs, .DELTA_vecs[[.i]]))
   
-  # TO DO hunt for NA
+  # browser()
   if (any(is.na(.maxLikLag))) {
     warning('some values of maxLikLag are NaN: you might want to increase the scaling factor')
     return(list())
     }
   .tau <- which.max(.maxLikLag) + .skip_start
   # case where the lag is longer than the data
-  if (.tau > length(.maxLikLag)-.skip_end) return(list(tau=Inf, x_lag=mean(.xs), slope_after=NA))
+  if (.tau > length(.maxLikLag)-.skip_end) {
+    # plot(1:.T,.xs, col=ifelse((1:.T)>=.tau, "red", "black"))
+    # lines(1:.T, c(mean(.xs)*rep(1,.T)))
+    return(list(tau_idx=Inf, x_lag=mean(.xs), slope_after=NA))
+  }
   .delta0 <- (.zs %*% .DELTA_vecs[[.tau-.skip_start]]) / (.DELTA_vecs[[.tau-.skip_start]] %*% .DELTA_vecs[[.tau-.skip_start]])
+  .delta0 <- .delta0 / .scaling_factor
   .x0 <- mean(.xs) - .delta0 * (.T-.tau) * (1+.T-.tau)/(2*.T)
   
   if (.plot) {
-    # plot(1:.T,.xs, col=ifelse((1:.T)>=.tau, "red", "black"))
-    # lines(1:.T, c(.x0*rep(1,.tau), .x0+seq(1,.T-.tau)*.delta0)) 
-    .pl <- qplot(1:.T,.xs, col=(1:.T)>=.tau) +
-      geom_line(aes(y=c(.x0*rep(1,.tau), .x0+seq(1,.T-.tau)*.delta0), col=NULL)) 
-      print(.pl)
+    plot(1:.T,.xs, col=ifelse((1:.T)>=.tau, "red", "black"))
+    lines(1:.T, c(.x0*rep(1,.tau), .x0+seq(1,.T-.tau)*.delta0))
+    # .pl <- qplot(1:.T,.xs, col=(1:.T)>=.tau) +
+    #   geom_line(aes(y=c(.x0*rep(1,.tau), .x0+seq(1,.T-.tau)*.delta0), col=NULL)) 
+    # print(.pl)
+    # return(list(tau_idx=.tau, x_lag=.x0, slope_after=.delta0, plot=.pl))
   }
   
-  return(list(tau=.tau, x_lag=.x0, slope_after=.delta0))
+  return(list(tau_idx=.tau, x_lag=.x0, slope_after=.delta0))
 }
 
